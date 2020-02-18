70952437ca2bdd0a472afce6ee0aff175a74f278
merothh <merothh@gmail.com>
2020-02-18 07:25:06 +0530

    testing

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,7 +11,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars top and bottom
 echo "---" | tee -a /tmp/polybar_top.log /tmp/polybar_bottom.log
-polybar top >>/tmp/polybar_top.log 2>&1 &
+#polybar top >>/tmp/polybar_top.log 2>&1 &
 polybar bottom >>/tmp/polybar_bottom.log 2>&1 &
 
 echo "Bars launched..."
